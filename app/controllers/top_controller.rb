8195e0977e999faefed1b437f00af4ea4096384d
tike <puodsea@gmail.com>
2016-10-17 14:43:34 +0900

    top#index（トップぺージ）作成とスタイル調整

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,5 @@
+class TopController < ApplicationController
+  def auth
+    # TODO: 認証処理
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'top#index'
+  post '/top/auth', 'top#auth'
   resource :users
 end
