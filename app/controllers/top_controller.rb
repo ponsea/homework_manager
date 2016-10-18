ee59709290686cad82f1e5410d207bc20a6acc06
tike <puodsea@gmail.com>
2016-10-18 15:58:16 +0900

    ユーザ認証の実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,9 +1,27 @@
 class TopController < ApplicationController
   def auth
-    # TODO: 認証処理
+    # 認証処理。成功した場合は当該Userオブジェクト。失敗した場合はnil
+    user = User.authenticate(params[:email], params[:password])
+
+    if user
+      # 認証が成功した場合はセッションidを発行
+      reset_session
+      session[:user_id] = user.id
+      if params[:referer].blank?
+        # リダイレクトによるログインでない場合はusers#showへ
+        redirect_to controller: :users, action: :show
+      else
+        # リダイレクトによるログインの場合は元の場所へ。
+        redirect_to params[:referer]
+      end
+    else
+      # 認証が失敗した場合はリダイレクト元を引き継いで#loginへ
+      flash[:referer] = params[:referer]
+      redirect_to top_login_path, alert: "メールアドレスまたはパスワードが間違っています"
+    end
   end
 
   def login
-    # TODO: ログインアクション
+    @title = "ログイン"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  # emailからユーザの存在を確認し、passwordで認証を行う。
+  # 認証に成功した場合は該当するUserオブジェクトを返す。
+  # 失敗した場合はnilを返す。
+  def self.authenticate(email, password)
+    if user = find_by(email: email)
+      digest = Digest::SHA256.hexdigest(password + user.salt)
+      if digest == user.password
+        return user
+      end
+    end
+    return nil
+  end
 end
