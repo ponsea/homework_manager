b5c2307524ccf38bf815ff72570e67d56a017aec
tike <puodsea@gmail.com>
2016-10-18 13:01:43 +0900

    ログインページ作成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,4 +2,8 @@ class TopController < ApplicationController
   def auth
     # TODO: 認証処理
   end
+
+  def login
+    # TODO: ログインアクション
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'top#index'
-  post '/top/auth', 'top#auth'
+  get '/top/login', to: 'top#login'
+  post '/top/auth', to: 'top#auth'
   resource :users
 end
