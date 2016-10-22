e393309945c57fbf5ad8b42b845aa8c43a5a27a1
tike <puodsea@gmail.com>
2016-10-22 20:04:56 +0900

    Userモデルのメソッドもろもろ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,21 @@ class User < ApplicationRecord
   # 失敗した場合はnilを返す。
   def self.authenticate(email, password)
     if user = find_by(email: email)
-      digest = Digest::SHA256.hexdigest(password + user.salt)
-      if digest == user.password
+      if user.digest_of(password) == user.password
         return user
       end
     end
     return nil
+  end
+
+  # ユーザのパスワードをハッシュ化する前に付与する文字列を生成する
+  # レインボーテーブル攻撃対策
+  def self.create_salt
+    SecureRandom.hex(8)
+  end
+
+  # パスワードを受け取り自身のソルトと合わせたうえでのハッシュ値を返す
+  def digest_of(password)
+    Digest::SHA256.hexdigest(password + self.salt)
   end
 end
