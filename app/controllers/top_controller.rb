415a99e1f8363dbd5f777f6d7253580322d35be7
tike <puodsea@gmail.com>
2016-10-22 21:41:44 +0900

    ログイン失敗時入力してたメールアドレス次回に持ち越しする

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -11,14 +11,15 @@ class TopController < ApplicationController
       session[:user_id] = user.id
       if params[:referer].blank?
         # リダイレクトによるログインでない場合はusers#showへ
-        redirect_to controller: :users, action: :show
+        redirect_to user_path
       else
         # リダイレクトによるログインの場合は元の場所へ。
         redirect_to params[:referer]
       end
     else
-      # 認証が失敗した場合はリダイレクト元を引き継いで#loginへ
+      # 認証が失敗した場合はリダイレクト元を引き継いでtop#loginへ
       flash[:referer] = params[:referer]
+      flash[:email] = params[:email]
       redirect_to top_login_path, alert: "メールアドレスまたはパスワードが間違っています"
     end
   end
