6142dcf44f3cf61a2c806eede3dadb4309db152d
tike <puodsea@gmail.com>
2016-10-26 11:31:33 +0900

    ユーザ新規登録処理の実装 & 登録完了画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :check_logined, except: :new
-  before_action :check_not_logined, only: :new
+  before_action :check_logined, except: [:new, :create]
+  before_action :check_not_logined, only: [:new, :create]
 
   def show
     @title = "ホーム"
@@ -15,6 +15,19 @@ class UsersController < ApplicationController
     @new_user = User.new
   end
 
+  def create
+    @new_user = User.new(users_params)
+
+    if @new_user.save
+      reset_session
+      session[:user_id] = @new_user.id
+      @user = @new_user
+    else
+      render :new
+    end
+  end
+
+
   def logout
     reset_session
     redirect_to root_path
@@ -28,5 +41,14 @@ class UsersController < ApplicationController
       flash[:referer] = request.fullpath
       redirect_to controller: :top, action: :login
     end
+  end
+
+  def users_params
+    params.require(:user).permit(
+      :email,
+      :validate_password,
+      :validate_password_confirmation,
+      :username
+    )
   end
 end
