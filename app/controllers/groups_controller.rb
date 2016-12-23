79a304f73b2a87d84e95f3c0bc86b054fcdef7f0
tike <puodsea@gmail.com>
2016-12-23 22:04:02 +0900

    グループ作成処理の実装 & 作成完了ページ作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,36 @@ class GroupsController < ApplicationController
   def new
     @new_group = Group.new
   end
+
+  def create
+    #ユーザが入力した値を取得
+    params = groups_params
+    # グループの作成者を設定（ログインユーザ）
+    params.store :author, logined_user
+    # グループオブジェクト作成
+    @new_group = Group.new(params)
+
+    # ユーザの入力に誤りがあった場合は作成ページに戻る
+    unless @new_group.valid?
+      render :new
+      return
+    end
+
+    # 作成したグループをDBに反映する。同時に、作成者をそのグループのメンバーとする
+    Group.transaction do
+      @new_group.save! validate: false
+      UsersGroup.create!(user: @new_group.author, group: @new_group, admin: true)
+    end
+  end
+
+  private
+  def groups_params
+    params.require(:group).permit(
+      :name,
+      :detail,
+      :private,
+      :password,
+      :password_confirmation
+    )
+  end
 end
