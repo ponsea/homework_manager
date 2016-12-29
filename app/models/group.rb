9212ebebd385114fa559fa2262c503e813d5115c
tike <puodsea@gmail.com>
2016-12-29 14:15:04 +0900

    Groupモデルのadminsアソシエーションの抽出条件の表現変更
    前の表現は互換性がなさそうな気がしたため

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :members, join_table: :users_groups, class_name: 'User'
-  has_and_belongs_to_many :admins, -> {where ['users_groups.admin = ?', true]}, join_table: :users_groups, class_name: 'User'
+  has_and_belongs_to_many :admins, -> { where(users_groups: {admin: true}) }, join_table: :users_groups, class_name: 'User'
 
   # *** バリデーション ***
   validates :name,
