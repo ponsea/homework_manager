b37357bd249ad11d91f989d58844107ce31e5756
tike <puodsea@gmail.com>
2017-01-14 16:53:53 +0900

    Taskモデルのバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,18 @@
 class Task < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :group
+
+  # *** バリデーション ***
+  validates :title,
+    presence: { message: '必須です'},
+    length: { maximum: 120, message: 'タイトルは120文字以内です'}
+  validates :points,
+    inclusion: {
+      in: 0..30000,
+      allow_nil: true
+    }
+  validates :importance,
+    inclusion: {
+      in: 1..10,
+    }
 end
