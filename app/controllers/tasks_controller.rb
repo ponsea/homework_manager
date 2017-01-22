e50aa6ef9eba48aadff2f3588e73d27779d4c3c1
tike <puodsea@gmail.com>
2017-01-22 16:58:36 +0900

    タスク作成画面作成 & タスク作成処理実装
    メール送信機能は未実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,31 @@ class TasksController < OnGroupsController
   def index
     @tasks = Task.where(group: @group).order(updated_at: :desc)
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    if @task.save
+      # TODO: メール送信処理
+
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(
+      :title,
+      :detail,
+      :deadline,
+      :importance,
+      :points
+    ).merge(group: @group, author: @user)
+  end
 end
